33f88c5d5c6446baa8dc28af2dcd49476acfef74
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-11-24 21:14:19 -0800

    Send a message on root

--- routes/LogInfoRoutes.js
+++ routes/LogInfoRoutes.js
@@ -3,6 +3,10 @@ import LogInModel from '../models/LogInfoModel.js';
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World!</h1>");
+});
+
 app.post("/add_info", async (req, res) => {
     const info = new LogInModel(req.body);
 
